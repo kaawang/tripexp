9034ce478d8afec07d9688050da298f68e6ebeb8
William Tsang <williamkt91@gmail.com>
2015-03-31 14:42:46 -0700

    Form capture completed, autocomplete suggestion is captured, controller contains tripdetails and http request packaging

--- app/views/trip/trip.js
+++ app/views/trip/trip.js
@@ -9,11 +9,51 @@ angular.module('tripexp.trip', ['ngAutocomplete', 'pickadate'])
   });
 }])
 
-.controller('TripCtrl', function($scope) {
-  $scope.result = '';
+.controller('TripCtrl', function($scope, $http) {
+  
+  // NGAUTOCOMPLETE - TRIP LOCATION FIELD
+
   $scope.options = null;
   $scope.details = '';
-});
+
+  $scope.tripdetails = {};
+
+  // DATE ORGANIZER
+  var date = new Date();
+  var dateArray = date.toString().split(" ");
+  if (dateArray[1] === "Oct" || dateArray[1] === "Nov" || dateArray[1] === "Dec") {
+    var month = "-1";
+  } else {
+    var month = "-0";
+  }
+  var dateString = dateArray[3] + month + (date.getMonth()+1) + "-" + dateArray[2]
+  $scope.minDate = dateString;
+  $scope.maxDate = "3000-12-31";
+
+  // POSTING TRIP DETAILS TO API
+  $scope.createTrip = function(tripdetails){
+    var data = {
+       tripName: tripdetails.name,
+       tripLocation: tripdetails.location,
+       tripStartDate: tripdetails.startDate,
+       tripEndDate: tripdetails.endDate
+    };
+    console.log(data);
+    var request = {
+      method: "POST",
+      url: "https://localhost:3000/api/users/1/trips",
+      data: data
+    }
+    console.log(request)
+    $http(request).success(function(response){
+      console.log("success")
+      console.log(response)
+    }).error(function(response){
+      console.log("error")
+      console.log(response)
+    })
+  }
+});  // End of controller
 
 
 
